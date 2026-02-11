0d6b778c280b77d0d2ddd817224ece0b2bcee665
Thomas Ha <thomasha.1310@gmail.com>
2026-02-11 10:16:36 -0500

    Code chunks

---
+++ chunk.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include "chunk.h"
+#include "memory.h"
+
+void initChunk(Chunk* chunk) {
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void freeChunk(Chunk* chunk) {
+    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    initChunk(chunk);
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+    if (chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = oldCapacity < 8 ? 8 : oldCapacity * 2;
+        chunk->code = realloc(chunk->code, sizeof(uint8_t) * chunk->capacity);
+    }
+
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
+}
