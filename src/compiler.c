0f5cbe16db7299fd9a2c0e20d4de50cb2dc65e52
Thomas Ha <thomasha.1310@gmail.com>
2026-02-12 18:46:50 -0500

    Initial scaffolding for interpreter and compiler

---
+++ src/compiler.c
@@ -0,0 +1,8 @@
+#include "compiler.h"
+
+#include <stdio.h>
+
+#include "common.h"
+#include "scanner.h"
+
+void compile(const char* source) { initScanner(source); }
--- src/vm.c
+++ src/vm.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 
 VM vm;
@@ -84,8 +85,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
