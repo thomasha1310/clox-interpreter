49711c761420a7a9da0f0634296a6d657f7b98c6
Thomas Ha <thomasha.1310@gmail.com>
2026-02-17 18:45:20 -0500

    Debug chunk dumping

--- src/compiler.c
+++ src/compiler.c
@@ -6,6 +6,10 @@
 #include "common.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
 typedef struct {
     Token current;
     Token previous;
@@ -106,7 +110,14 @@ static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
-static void endCompiler() { emitReturn(); }
+static void endCompiler() {
+    emitReturn();
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.hadError) {
+        disassembleChunk(currentChunk(), "code");
+    }
+#endif
+}
 
 static void expression();
 static ParseRule* getRule(TokenType type);
