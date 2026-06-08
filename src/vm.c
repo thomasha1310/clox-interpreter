14d26567352f36e4030c655f5f7a79ecb2907275
Thomas Ha <thomasha.1310@gmail.com>
2026-06-08 15:23:05 -0400

    Add runtime error checking for function calls

--- src/vm.c
+++ src/vm.c
@@ -66,6 +66,17 @@ Value pop() {
 static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
 
 static bool call(ObjFunction* function, int argCount) {
+    if (argCount != function->arity) {
+        runtimeError("Expect %d arguments but got %d.", function->arity,
+                     argCount);
+        return false;
+    }
+
+    if (vm.frameCount == FRAMES_MAX) {
+        runtimeError("Stack overflow.");
+        return false;
+    }
+
     CallFrame* frame = &vm.frames[vm.frameCount++];
     frame->function = function;
     frame->ip = function->chunk.code;
